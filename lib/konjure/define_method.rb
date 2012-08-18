463e7bf5f6d638aa9d1ccfafef8d0f4ff4b8a3e5
Dave Lee <davelee.com@gmail.com>
2012-08-18 17:36:09 -0600

    Wrap define_method procs before passing to class_replaceMethod()
    Refactor to wrap method body procs in a another proc before being
    passed to the Objective-C runtime.
    
    The reason for wrapping the method body proc is to prepend two
    necessary Objective-C parameters, self and cmd (selector).
    
    Because procs need a correct signature/arity, `method_implementation`,
    the factory method that wraps the original proc, has a non-DRY case
    statement which spawns wrapper procs with explicitly correct arity.

--- lib/konjure/define_method.rb
+++ lib/konjure/define_method.rb
@@ -2,35 +2,68 @@ module Module
 
   # Overwrite the existing useless implementation
   def define_method(name, &block)
-    raise ArgumentError, 'Block required' if block.nil?
-
-    signature = Konjure::objc_parameter_signature(block)
-    objc_define_method(name, :object, signature, &block)
+    objc_define_method(name, :object, Konjure::objc_parameter_types(block), &block)
   end
 
-  def objc_define_method(name, *types, &block)
-    return_type, *parameter_types = Konjure::encode_objc_types(types)
-    return_type ||= '@'
-    parameter_types = parameter_types.join('')
+  def objc_define_method(name, return_type = :object, *parameter_types, &block)
+    raise ArgumentError, 'Block required' if block.nil?
 
-    signature = "#{return_type}@:#{parameter_types}"
-    class_replaceMethod(self, name.to_sym, block, signature.UTF8String)
-
-    block
+    signature = Konjure::objc_signature(return_type, parameter_types)
+    implementation = Konjure::method_implementation(block)
+    implementation.retain # prevent RubyMotion from collecting the proc
+    class_replaceMethod(self, name.to_sym, implementation, signature.UTF8String)
+    implementation
   end
 
 end
 
 module Konjure
 
-  def self.encode_objc_types(types)
-    types.flatten.map do |type|
+  def self.objc_signature(*types)
+    return_type, *parameter_types = types.flatten.map { |type|
       TYPE_SHORTCUTS[type] || type
-    end
+    }
+    return_type ||= '@'
+    parameter_types = parameter_types.join('')
+
+    "#{return_type}@:#{parameter_types}"
   end
 
-  def self.objc_parameter_signature(block)
-    '@' * block.arity.abs
+  def self.objc_parameter_types(block)
+    [:object] * block.arity.abs
+  end
+
+  def self.method_implementation(block)
+    case block.arity
+    when 0
+      lambda { |_self, cmd| _self.instance_exec(&block) }
+    when 1
+      lambda { |_self, cmd, a| _self.instance_exec(a, &block) }
+    when -1
+      lambda { |_self, cmd, *a| _self.instance_exec(*a, &block) }
+    when 2
+      lambda { |_self, cmd, a,b| _self.instance_exec(a,b, &block) }
+    when -2
+      lambda { |_self, cmd, a,*b| _self.instance_exec(a,*b, &block) }
+    when 3
+      lambda { |_self, cmd, a,b,c| _self.instance_exec(a,b,c, &block) }
+    when -3
+      lambda { |_self, cmd, a,b,*c| _self.instance_exec(a,b,*c, &block) }
+    when 4
+      lambda { |_self, cmd, a,b,c,d| _self.instance_exec(a,b,c,d, &block) }
+    when -4
+      lambda { |_self, cmd, a,b,c,*d| _self.instance_exec(a,b,c,*d, &block) }
+    when 5
+      lambda { |_self, cmd, a,b,c,d,e| _self.instance_exec(a,b,c,d,e, &block) }
+    when -5
+      lambda { |_self, cmd, a,b,c,d,*e| _self.instance_exec(a,b,c,d,*e, &block) }
+    when 6
+      lambda { |_self, cmd, a,b,c,d,e,f| _self.instance_exec(a,b,c,d,e,f, &block) }
+    when -6
+      lambda { |_self, cmd, a,b,c,d,e,*f| _self.instance_exec(a,b,c,d,e,*f, &block) }
+    else
+      raise ArgumentError, 'define_method is limited to methods with 0..6 parameters'
+    end
   end
 
   TYPE_SHORTCUTS = {
